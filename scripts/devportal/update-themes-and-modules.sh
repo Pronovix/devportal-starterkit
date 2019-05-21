7b5da3da408e9e748ddee72da584c436af19851c
Dezső BICZÓ <mxr576@gmail.com>
2019-05-21 09:42:00 +0200

    Pull the latest tagged release
    Plus other code quality improvements.

--- scripts/devportal/update-themes-and-modules.sh
+++ scripts/devportal/update-themes-and-modules.sh
@@ -1,55 +1,81 @@
 #!/bin/bash
-# update-px-modules.sh
 
-# UPDATE ALL PRONOVIX MODULES
+# update-themes-and-modules.sh
+# UPDATE ALL PRONOVIX MODULES AND THEMES.
 
 # ----- CONSTANTS ------
 declare -r MODULES_DIR='web/modules/devportal'
 declare -r THEMES_DIR='web/themes/devportal'
 declare -a modules
 declare -a themes
+declare -r DEVPORTAL_REMOTE_REPOSITORY_URL=${DEVPORTAL_REMOTE_REPOSITORY_URL-:"git@bitbucket.org:pronovix"}
 
-for dir in ./${MODULES_DIR}/*/; do
-    modules+=("$(basename "${dir}")")
-done
+if [[ -d ${MODULES_DIR} ]]; then
+  for dir in $(find ${MODULES_DIR} -mindepth 1 -maxdepth 1 -type d); do
+    modules+=( "$(basename "${dir}")" )
+  done
+fi
 
-for dir in ./${THEMES_DIR}/*/; do
-    themes+=("$(basename "${dir}")")
-done
+if [[ -d ${THEMES_DIR} ]]; then
+  for dir in $(find ${THEMES_DIR} -mindepth 1 -maxdepth 1 -type d); do
+    echo $dir;
+    themes+=( "$(basename "${dir}")" )
+  done
+fi
 
 function add_all_repositories {
-    add_all_modules_repositories
-    add_all_themes_repositories
+  add_all_modules_repositories
+  add_all_themes_repositories
 }
 
 function add_all_modules_repositories {
-    for repo_name in ${modules[@]}
-    do
-        git remote add -f ${repo_name} git@bitbucket.org:pronovix/${repo_name}.git
-    done
+  for repo_name in ${modules[@]}
+  do
+    register_repository ${repo_name}
+  done
 }
 
 function add_all_themes_repositories {
-    for repo_name in ${themes[@]}
-    do
-        git remote add -f ${repo_name} git@bitbucket.org:pronovix/${repo_name}.git
-    done
+  for repo_name in ${themes[@]}
+  do
+    register_repository ${repo_name}
+  done
+}
+
+function register_repository {
+  local repo_name=$1
+  git remote get-url ${repo_name} &> /dev/null
+  if [[ $? -gt 0 ]]; then
+    git remote add -f ${repo_name} ${DEVPORTAL_REMOTE_REPOSITORY_URL}/${repo_name}.git
+  fi
 }
 
 function update_all_modules {
-    for repo_name in ${modules[@]}
-    do
-        git fetch ${repo_name}
-        git subtree pull -P ${MODULES_DIR}/${repo_name} --squash ${repo_name} 8.x-1.x
-    done
+  for repo_name in ${modules[@]}
+  do
+    pull_latest_tag ${repo_name} ${MODULES_DIR}
+  done
 }
 
 function update_all_themes {
-    for repo_name in ${themes[@]}
-    do
-        git fetch ${repo_name}
-        git subtree pull -P ${THEMES_DIR}/${repo_name} --squash ${repo_name} 8.x-1.x
-    done
+  for repo_name in ${themes[@]}
+  do
+    pull_latest_tag ${repo_name} ${THEMES_DIR}
+  done
+}
+
+function pull_latest_tag {
+  local repo_name=$1
+  local directory=$2
+  git fetch ${repo_name}
+  git ls-remote --tags --refs -q --exit-code ${repo_name} > /dev/null
+  if [[ $? -eq 0 ]]; then
+    # Extract "8.x-1.0-alpha1" from "refs/tags/8.x-1.0-alpha1".
+    local latest_tag=$(git ls-remote --tags --refs ${repo_name} | tail -n 1 | awk -F '/' '{print $3}')
+    git subtree pull --prefix=${directory}/${repo_name} --squash ${repo_name} ${latest_tag} -m "${repo_name} ${latest_tag}"
+  else
+    echo -e "${repo_name} does not have any tagged release.\n"
+  fi
 }
 
 git stash -u
